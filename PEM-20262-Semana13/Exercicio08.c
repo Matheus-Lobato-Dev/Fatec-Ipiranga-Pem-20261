c5e371ee6b8279386857eb9c76616928003eea0f
Matheus-Lobato-Dev <lobatomatheus.44@gmail.com>
2026-05-15 15:48:23 -0300

    Create Exercicio08.c
    Implement main function to calculate difference between two pointers in an array.

---
+++ PEM-20262-Semana13/Exercicio08.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double v[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    double *p3 = &v[2];
+    double *p8 = &v[7];
+
+    printf("Diferenca (p8 - p3): %ld elementos\n", p8 - p3);
+
+    (sizeof(double)), resultando no número de elementos entre eles[cite: 86, 100].
+    return 0;
+}
